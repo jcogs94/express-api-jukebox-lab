5db26e626ebb55e9688b1a37c0b146c5b64311b2
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-17 14:19:30 -0400

    server init, model added

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Conneted to MongoDB - ${mongoose.connection.name}`)
+})
+
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...')
+})
