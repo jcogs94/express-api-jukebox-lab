99b8d39daf0e336849db8a2dc307f4f442bf9b36
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-17 14:54:21 -0400

    all 5 routes

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -4,9 +4,9 @@ const tracksCtrl = require('./tracksCtrl.js')
 
 // ROUTES
 router.post('/', tracksCtrl.create)
-// router.get('/', tracksCtrl.index)
-// router.get('/:trackId', tracksCtrl.show)
-// router.put('/:trackId', tracksCtrl.update)
-// router.delete('/:trackId', tracksCtrl.delete)
+router.get('/', tracksCtrl.index)
+router.get('/:trackId', tracksCtrl.show)
+router.put('/:trackId', tracksCtrl.update)
+router.delete('/:trackId', tracksCtrl.del)
 
 module.exports = router
--- controllers/tracksCtrl.js
+++ controllers/tracksCtrl.js
@@ -5,11 +5,46 @@ const create = async (req, res) => {
         const newTrack = await Track.create(req.body)
         res.status(201).json(newTrack)
     } catch (error) {
-        console.error(error)
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const index = async (req, res) => {
+    try {
+        const allTracks = await Track.find()
+        res.status(200).json(allTracks)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const show = async (req, res) => {
+    try {
+        const foundTrack = await Track.findById(req.params.trackId)
+        res.status(200).json(foundTrack)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const foundTrack = await Track.findByIdAndUpdate(req.params.trackId)
+        res.status(200).json(foundTrack)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const del = async (req, res) => {
+    try {
+        const foundTrack = await Track.findByIdAndDelete(req.params.trackId)
+        res.status(200).json(foundTrack)
+    } catch (error) {
         res.status(500).json({ error: error.message })
     }
 }
 
 module.exports = {
-    create
+    create, index, show, update, del
 }
