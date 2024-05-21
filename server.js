950587d5ede76d22794341a8e51e34e0ba882ced
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-21 11:28:37 -0400

    change port

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(express.json())
 const trackRouter = require('./controllers/tracks.js')
 app.use('/tracks', trackRouter)
 
-app.listen(3009, () => {
-    console.log('Listening on port 3009...')
+app.listen(3008, () => {
+    console.log('Listening on port 3008...')
 })
